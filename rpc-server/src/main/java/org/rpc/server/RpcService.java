024ae7b7c40daf2c2003de96a02be71c365cbca7
周星航 <xinghang.zhou@renren-inc.com>
2017-09-22 16:40:17 +0800

    添加注解类，由Spring扫描

--- rpc-server/src/main/java/org/rpc/server/RpcService.java
+++ rpc-server/src/main/java/org/rpc/server/RpcService.java
@@ -1,7 +1,23 @@
 package org.rpc.server;
 
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by xinghang on 17/9/22.
+ * RPC服务注解类，标注在服务实现类中
  */
-public interface RpcService {
+
+@Target({ElementType.TYPE})//注解用在接口上
+@Retention(RetentionPolicy.RUNTIME)//VM将在运行期也保留注释，因此可以通过反射机制读取注释的信息
+@Component
+public @interface RpcService {
+  /**
+   * 服务接口类
+   */
+  Class<?> value();
 }
