762e753cce9bc9ad6f7232bad91ae4cc91e695e2
周星航 <xinghang.zhou@renren-inc.com>
2017-09-25 16:25:53 +0800

    新增测试类

--- rpc-sample-client/src/main/java/org/rpc/sample/client/HelloClient.java
+++ rpc-sample-client/src/main/java/org/rpc/sample/client/HelloClient.java
@@ -1,7 +1,26 @@
 package org.rpc.sample.client;
 
+import org.rpc.client.RpcProxy;
+import org.rpc.sample.api.HelloService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by xinghang on 17/9/25.
  */
 public class HelloClient {
+  public static void main(String[] args) throws Exception {
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    RpcProxy rpcProxy = context.getBean(RpcProxy.class);
+
+    HelloService helloService = rpcProxy.create(HelloService.class);
+    String result = helloService.hello("World");
+    System.out.println(result);
+
+    HelloService helloService2 = rpcProxy.create(HelloService.class);
+    String result2 = helloService2.hello("世界");
+    System.out.println(result2);
+
+    System.exit(0);
+  }
 }
